368eef567a093dacf8ccd57c46b5427d269385dd
ABES-CSE <emailoabes@gmail.com>
2026-02-20 22:06:02 +0530

    Initial commit

---
+++ 06-booleans.js
@@ -0,0 +1,10 @@
+console.log(3==='3');
+true;
+false;
+console.log(typeof true);
+
+if(21>=18){
+    console.log(`You are ready to drive.`);
+} else{
+    console.log(`You are not ready to drive.`);
+}
