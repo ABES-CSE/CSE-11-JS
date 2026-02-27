7eb56bfd70e1b30749e9a5902561c926b8498fc1
ABES-CSE <emailoabes@gmail.com>
2026-02-27 12:18:03 +0530

    Add my Codes

---
+++ JKC-02(1).js
@@ -0,0 +1,11 @@
+let a=9,b=5,c=4;
+(a>b && a>c)?console.log(a):(b>c)? console.log(b):console.log(c);
+
+for(i=0;i<3;i=i+1){
+    console.log(i);
+}
+
+let count;
+for(count=0;count<10;count++){
+    console.log(count);
+}
---
+++ LearningJS/new.js
@@ -0,0 +1,6 @@
+let student={name: `Rohit Marcopolo`, age: 89, cgpa: 87.56, isPass: true};
+console.log(student.cgpa);
+student.age+=1;
+console.log(student.age);
+student.name="Sun Tzu";
+console.log(student.name);
